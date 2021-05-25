faf049e016631de64f8e55cb88bfe36bdfb786b0
yonggyo1981 <yonggyo1981@gmail.com>
2021-05-25 12:03:56 +0900

    .

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const dotenv = require('dotenv');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const { sequelize } = require("./models");
+
+/** 라우터 */
+const memberRouter = require('./routes/member');
 
 dotenv.config();
 
@@ -16,6 +20,15 @@ nunjucks.configure("views", {
 	express : app,
 	watch : true,
 });
+
+/** DB 연결 */
+sequelize.sync({ force : false })
+			.then(() => {
+				console.log("데이터베이스 연결 성공!");
+			})
+			.catch((err) => {
+				console.error(err);
+			});
 
 app.use(morgan('dev'));
 app.use(methodOverride("_method"));
@@ -34,6 +47,8 @@ app.use(session({
 }));
 
 
+/** 라우터 등록 */
+app.use("/member", memberRouter);
 
 // 없는 페이지 처리 미들웨어(라우터)
 app.use((req, res, next) => {
---
+++ routes/member.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.route("/join")
+		/** 회원 가입 양식 */
+		.get((req, res, next) => {
+			
+		})
+		/** 회원 가입 처리 */
+		.post((req, res, next) => {
+			
+		});
+
+module.exports = router;
