1be9962b0f5b44f19a005ddfaac5106e1d275e80
yonggyo1981 <yonggyo1981@gmail.com>
2021-05-25 12:21:52 +0900

    라우터 구성, 기본 템플릿 설정

--- routes/member.js
+++ routes/member.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.route("/join")
 		/** 회원 가입 양식 */
 		.get((req, res, next) => {
-			
+			res.render("member/form");
 		})
 		/** 회원 가입 처리 */
 		.post((req, res, next) => {
