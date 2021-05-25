7e3a5daa4a1acdee8118b1008b297b0cf7ff81ec
yonggyo1981 <yonggyo1981@gmail.com>
2021-05-25 13:02:14 +0900

    .

--- middlewares/join_validator.js
+++ middlewares/join_validator.js
@@ -1,16 +1,17 @@
+const { alert } = require('../lib/common');
+
 /**
 * 회원 가입 유효성 검사 
 */
-
 module.exports.joinValidator = (req, res, next) => {
-	// req.body.memId, req.body.memPw
+	// 필수 입력항목 체크 
 	if (!req.body.memId) {
-		
+		return alert("아이디를 입력하세요.", res);
 	}
 	
 	if (!req.body.memPw) {
-		
+		return alert("비밀번호를 입력하세요.", res);
 	}
 	
-	next(); // 다음 미들웨어로 이동
+	next(); // 유효성 검사 성공시 -> 다음 미들웨어로 이동
 };
