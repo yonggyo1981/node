ef3419e62a4162336e300c66875203ca841317c2
yonggyo1981 <yonggyo1981@gmail.com>
2021-05-26 15:37:58 +0900

    .

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,3 @@
-
 /**
 * 프로필 사진 업로드 처리
 *
@@ -37,5 +36,27 @@ $(function() {
 				$("input[name='address']").val(data.address);
 			}
 		}).open();
+	});
+	
+	/** 양식 추가 처리 */
+	$(".add_form").click(function() {
+		const type = $(this).data("type");
+		let template = "";
+		switch (type) {
+			case "학력" : 
+				template = "school";
+				break;
+		}
+		
+		if (template) {
+			$html = $("#template_" + template).html();
+			$target = $(this).closest(".form_inner").find(".form_html");
+			$target.append($html);
+		} 
+	});
+	
+	/** 양식 제거 처리 */
+	$("body").on("click", ".form_html .remove", function() {
+		$(this).closest(".rows").remove();
 	});
 });
