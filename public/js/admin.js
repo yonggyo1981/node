6f3cd7dae5f70a814d6008fe534a48ee35d43bb9
yonggyo1981 <yonggyo1981@gmail.com>
2021-05-26 12:57:25 +0900

    .

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,10 @@
 
 $(function() {
+	/** 파일 업로드 처리 */
+	$("body").on("change", ".upload_box input[type='file']", function() {
+		frmUpload.submit();
+	});
+	
 	/** 주소 검색 */
 	$(".search_address").click(function() {
 		 new daum.Postcode({
