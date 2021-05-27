bd998866b5bb84990d7b203221404c212dbd7c60
yonggyo1981 <yonggyo1981@gmail.com>
2021-05-27 15:04:40 +0900

    .

--- models/resume.js
+++ models/resume.js
@@ -11,8 +11,51 @@ const resume = {
 	* @return Boolean 
 	*/
 	update : async function(params) {
+		console.log(params);
 		try {
-			
+			// basicinfo 처리  S 
+			/**
+			 basicInfo
+  취업우대,병역
+  beneift 
+  handicapLevel: '',
+  military: '',
+			*/
+			let sql = `UPDATE basicinfo 
+									SET 
+										  resumeTitle = :resumeTitle, 
+										  name = :name,
+										  birthDate = :birthDate,
+										  landline = :landline,
+										  cellPhone = :cellPhone,
+										  gender = :gender,
+										  email = :email,
+										  address = :address,
+										  workType = :workType,
+										  salary = :salary,
+										  workLocation = :workLocation,
+										  workKeyword = :workKeyword,
+										  beneift = :beneift,
+										  handicapLevel = :handicapLevel,
+										  military = :military`;
+				let replacements = {
+					resumeTitle : params.resumeTitle,
+					name : params.name,
+					birthDate : params.birthDate,
+					landline : params.landline,
+					cellPhone : params.cellPhone,
+					gender : params.gender,
+					email : params.email,
+					address : params.address,
+					workType : params.workType,
+					salary : params.salary,
+					workLocation : params.workLocation,
+					workKeyword : params.workKeyword,
+					beneift : params.beneift,
+					handicapLevel : params.handicapLevel,
+					military : params.military,
+				};
+			// basicinfo 처리 E 
 			
 			return true;
 		} catch (err) {
