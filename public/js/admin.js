83865f65141268fb7fe6501b1928c744d49a8b6b
yonggyo1981 <yonggyo1981@gmail.com>
2021-05-27 11:10:46 +0900

    .

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,29 @@
+/** 
+* 취업우대,병역 항목 선택시 
+* 장애, 병역 선택에 따른 항목 노출 
+*
+*/
+function updateBenefit()
+{
+	$list = $(".benefit input[type='checkbox']:checked");
+	let isAdditionalSelect = false;
+	$(".additional_select, .additional_select dl").removeClass("dn").addClass("dn");
+	$.each($list, function() {
+		const benefit = $(this).val();
+		if (benefit == '장애') {
+			isAdditionalSelect = true;
+			$(".additional_select .handicap").removeClass("dn");
+		} else if (benefit == '병역') {
+			isAdditionalSelect = true;
+			$(".additional_select .military").removeClass("dn");
+		}
+	});
+	
+	if (isAdditionalSelect) {
+		$(".additional_select").removeClass("dn");
+	}
+}
+
 /**
 * 프로필 사진 업로드 처리
 *
@@ -90,10 +116,18 @@ $(function() {
 	
 	/** textarea 확대 축소 처리 */
 	$("body").on("focus", ".form_html textarea", function() {
-		$(this).removeClass("h200").addClass("h200");
+		if (!$(this).hasClass("intro")) {
+			$(this).removeClass("h200").addClass("h200");
+		}
 	});
 	
 	$("body").on("blur", ".form_html textarea", function() {
 		$(this).removeClass("h200");
 	});
+	
+	/** 취업우대, 병역 클릭 처리 */
+	$(".benefit input[type='checkbox']").click(function() {
+		updateBenefit();
+	});
+	
 });
